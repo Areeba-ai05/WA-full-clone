91da28fd6b19a9c422391b96e1251cfad7676166
Areeba Iqbal <areeba.seo05@gmail.com>
2024-12-25 22:38:14 +0500

    update code

---
+++ lib/View/homeview/chats-homeview/specific-chat-view/chathome-view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SpecificChat extends StatelessWidget {
+  const SpecificChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
